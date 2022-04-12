da3c92395b871ba915e612317fe6093ffb72b335
sudhanshu <jhasudhanshu08@gmail.com>
2022-04-12 11:45:36 +0530

    new

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+require("./db/connection")
+const app = express();
+const bodyParser = require("express").json
+const UserRouter = require("./api/User");
+
+app.use(bodyParser());
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) =>{
+    res.send("Hello");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.CONNECTDB, {
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Database is connected !!");
+}).catch(err => {
+    console.log("Ohh No Error", err);
+})
